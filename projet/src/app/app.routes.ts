b50c208bf175c1e814c1de471640b3ae274600f1
S0ul3y <maigasouleymane244@gmail.com>
2024-07-31 15:37:44 +0000

    adding of project

---
+++ projet/src/app/app.routes.ts
@@ -0,0 +1,21 @@
+import { Routes } from '@angular/router';
+// import { AppComponent } from './app.component';
+// import { NavbarComponent } from './Utilisateur/utilisateur.component';
+// import { EvenementComponent } from './evenement/evenement.component';
+// import { TacheComponent } from './Tache/tache.component';
+// import { PrestateurListComponent } from './prestateur-list/prestateur-list.component';
+// import { PrestateurComponent } from './prestateur/prestateur.component';
+// import { LoginComponent } from './login/login.component';
+// import { SidebarComponent } from './sidebar/sidebar.component';
+export const routes: Routes = [
+  // { path: '', component: LoginComponent },
+  // { path: 'utilisateur', component: NavbarComponent },
+  // { path: 'tache', component: TacheComponent },
+  // { path: 'evenement', component: EvenementComponent },
+  // { path: 'prestateur', component: PrestateurListComponent },
+  // { path: 'prestateurs', component: PrestateurListComponent },
+  // { path: 'add-prestateur', component: PrestateurComponent },
+  // { path: 'edit-prestateur/:id', component: PrestateurComponent },
+  // { path: 'sidebars', component: SidebarComponent},
+  // { path: 'A', component: n }
+];
